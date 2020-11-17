f7f83487bf12e3562fd00c5c51bbffb0c9dbf877
caochong <chong.cao@chinab2bi.com>
2020-11-17 20:38:32 +0800

    000

--- main.go
+++ main.go
@@ -1,34 +1,39 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"net/http"
 )
 
 func main() {
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "pong0",
-		})
-	})
-	r.GET("/add", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "add",
-		})
-	})
-	r.GET("/update", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "update",
-		})
-	})
-	r.GET("/delete", func(c *gin.Context) {
+	router := gin.Default()
+	router.GET("/ping", func(c *gin.Context) {
 		name := c.Query("name")
 		c.JSON(http.StatusOK, gin.H{
 			"message": name,
 		})
 	})
-	err := r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	// This handler will match /user/john but will not match /user/ or /user
+	router.GET("/user/:name", func(c *gin.Context) {
+		name := c.Param("name")
+		c.String(http.StatusOK, "Hello %s", name)
+	})
+
+	// However, this one will match /user/john/ and also /user/john/send
+	// If no other routers match /user/john, it will redirect to /user/john/
+	router.GET("/user/:name/*action", func(c *gin.Context) {
+		name := c.Param("name")
+		action := c.Param("action")
+		message := name + " is " + action
+		c.String(http.StatusOK, message)
+	})
+
+	// For each matched request Context will hold the route definition
+	router.POST("/user/:name/*action", func(c *gin.Context) {
+		fmt.Println(c.FullPath() == "/user/:name/*action") // true
+	})
+	err := router.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 	if err != nil {
 		panic(err)
 	}
